61094b0320a126125f59de1db50d76e6642e31db
mazhenyu997 <mazhenyu997@foxmail.com>
2018-06-15 19:55:36 +0800

    首页新闻排行的信息展示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -2,14 +2,14 @@
 from flask import render_template
 from flask import current_app
 
-from info.models import User
+from info.models import User, News
 from . import index_blu
 from flask import session
 from manage import app
 
 
 @index_blu.route('/')
-def hello_world():
+def index():
 
     user_id = session.get("user_id", None)
     user = None
@@ -19,9 +19,21 @@ def hello_world():
 
         except Exception as e:
             current_app.logger.error(e)
+    # 右侧新闻的逻辑
+    news_right_list = []
+    try:
+        news_right_list = News.query.order_by(News.clicks.desc()).limit(6)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 将列表中的对象转为列表中的字典
+    news_dict_list = []
+    for news in news_right_list:
+        news_dict_list.append(news.to_basic_dict())
 
     data = {
-        "user": user.to_dict() if user else None
+        "user": user.to_dict() if user else None,
+        "news_dict_li": news_dict_list
     }
     return render_template("news/index.html", data=data)
 
