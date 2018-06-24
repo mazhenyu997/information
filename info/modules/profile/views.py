abeaa6f4883867dec980b3ca087b01bd1e37ac44
mazhenyu997 <mazhenyu997@foxmail.com>
2018-06-24 13:28:52 +0800

    fix bug for 头像上传

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,3 +1,7 @@
+import os
+import random
+import time
+
 from info import db
 from info.constants import QINIU_DOMIN_PREFIX
 from info.modules.profile import profile_blu
@@ -55,26 +59,37 @@ def base_info():
 def pic_info():
     user = g.user
     if request.method == "GET":
-        return render_template('news/user_pic_info.html', data={"user_info": user.to_dict()})
+        return render_template('news/user_pic_info.html', data={"user": user.to_dict()})
 
     # 1. 获取到上传的文件
     try:
-        avatar_file = request.files.get("avatar").read()
+        avatar_file = request.files.get("avatar")
+
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.PARAMERR, errmsg="读取文件出错")
 
-    # 2. 再将文件上传到七牛云
+    # 2. 再将文件上传到七牛云，七牛账号失效，保存在本地
+    '''
     try:
         url = storage(avatar_file)
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.THIRDERR, errmsg="上传图片错误")
-
+    
+    try:
+        avatar_name = str(int(time.time())) + avatar_file.name
+        print(avatar_file.name)
+        destination = open(os.path.join("/Users/mazhenyu/Pictures/", avatar_name), 'wb+')
+        destination.write(avatar_file.read())
+        destination.close()
+    except Exception as e:
+        current_app.logger.error(e)
+    '''
     # 3. 将头像信息更新到当前用户的模型中
-
+    avatar_url = "http://chuantu.biz/t6/333/1529807592x-1404817844.jpg"
     # 设置用户模型相关数据
-    user.avatar_url = url
+    user.avatar_url = avatar_url
     # 将数据保存到数据库
     try:
         db.session.commit()
@@ -84,4 +99,4 @@ def pic_info():
         return jsonify(errno=RET.DBERR, errmsg="保存用户数据错误")
 
     # 4. 返回上传的结果<avatar_url>
-    return jsonify(errno=RET.OK, errmsg="OK", data={"avatar_url": QINIU_DOMIN_PREFIX + url})
+    return jsonify(errno=RET.OK, errmsg="OK", data={"avatar_url": avatar_url})
