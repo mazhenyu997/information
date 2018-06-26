73b7018ac13423411f5ee6bb48bee68bfe7c292e
mazhenyu997 <mazhenyu997@foxmail.com>
2018-06-26 14:40:21 +0800

    异常界面

--- info/__init__.py
+++ info/__init__.py
@@ -1,6 +1,6 @@
 from logging.handlers import RotatingFileHandler
 
-from flask import Flask
+from flask import Flask, render_template, g
 import logging
 from flask_wtf import CSRFProtect
 from flask_wtf.csrf import generate_csrf
@@ -51,6 +51,18 @@ def create_app(config_name):
     # 添加自定义过滤器
     app.add_template_filter(do_index_class, "index_class")
 
+    from info.utils.common import user_login_data
+
+    @app.errorhandler('/404')
+    @user_login_data
+    def page_not_fount(e):
+        user = g.user
+
+        data = {
+            "user": user.to_dict() if user else None
+        }
+        return render_template('news/404.html', data=data)
+
     @app.after_request
     def after_request(response):
         csrf_token = generate_csrf()
@@ -71,3 +83,4 @@ def create_app(config_name):
     app.register_blueprint(profile_blu)
 
     return app
+
