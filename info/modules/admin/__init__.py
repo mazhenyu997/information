39845a955ddbe5ead950552c2cd746cf665263af
mazhenyu997 <mazhenyu997@foxmail.com>
2018-06-28 12:30:04 +0800

    管理员登陆BUG修复

--- info/modules/admin/__init__.py
+++ info/modules/admin/__init__.py
@@ -1,8 +1,14 @@
-from flask import Blueprint
-
+from flask import Blueprint, session, redirect, request, url_for
 
 # 1 导入蓝图  2 创建蓝图对象  3 蓝图的路由
 
 admin_blu = Blueprint("admin", __name__, url_prefix='/admin')
 
 from . import views
+
+
+@admin_blu.before_request
+def check_admin():
+    is_admin = session.get("is_admin", False)
+    if not is_admin and not request.url.endswith(url_for("admin.login")):
+        return redirect("/")
